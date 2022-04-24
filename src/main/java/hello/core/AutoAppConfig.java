3405a37788d30093ccfcf9bc2e1493b52cc8ba70
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-04-24 22:50:26 +0900

    :sparkles: 자동 등록 빈과 수동 등록 빈 중복 발생 시나리오

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,12 @@ import org.springframework.context.annotation.FilterType;
 // 보통 실무에서 ANNOTATION과 Configuration을 제외하진 않는다
 // 지금은 제외 없이 진행하면 이전 config한 파일 예제들이 다 등록되기 때문에 제외한 것이다.
 // ComponentScan은 @Component 애노테이션이 붙은 클래스를 찾아서 스프링 빈으로 등록해준다
-public class AutoAppConfig {}
+public class AutoAppConfig {
+
+  // 수동 빈과 자동 등록 빈의 이름이 종복될 경우
+  // 수동으로 등록한 빈이 우선된다!!
+  @Bean(name = "memoryMemberRepository")
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+}
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoreApplication {
 
+  // 스프링 부트가 기본적으로 변경된 게 수동 등록 빈과 자동 등록 빈이 중복될 경우
+  // 기본적으로 오류를 발생하도록 기본 값을 바꾸었다!!
   public static void main(String[] args) {
     SpringApplication.run(CoreApplication.class, args);
   }
