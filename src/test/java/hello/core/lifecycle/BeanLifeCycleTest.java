6dbcf415b454cc038fdae92066ffd3193a411daa
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-04-26 15:07:07 +0900

    :memo: 빈 생명주기 관련 참고 사항 README update

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -17,6 +17,10 @@ public class BeanLifeCycleTest {
   }
 
   // 실행해 보면 url이 초기화되기 전에 메서드들이 호출되므로 url 값이 다 null로 나오게 된다
+  // 따라서 개발자는 의존관계 주입이 끝나고 초기화를 진행해야 한다
+  // 어떻게 의존관계 주입이 끝났는지 알 수 있을까?
+  // 스프링 빈의 이벤트 라이프사이클
+  // 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
   @Configuration
   static class LifeCycleConfig {
     @Bean
