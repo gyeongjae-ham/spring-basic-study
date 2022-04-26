c4ba3230d76278f7d0307fb1777ddd5ea0f63e1a
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-04-26 15:17:23 +0900

    :sparkles: 초기화, 소멸 인터페이스 사용하는 경우

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
   private String url;
 
   public NetworkClient() {
     System.out.println("생성자 호출, url = " + url);
-    connect();
-    call("초기화 연결 메시지");
   }
 
   public void setUrl(String url) {
@@ -26,5 +27,23 @@ public class NetworkClient {
   // 서비스 종료 시 호출
   public void disconnect() {
     System.out.println("close " + url);
+  }
+
+  /**
+   * 이 인터페이스는 스프링 전용 인터페이스고, 해당 코드가 스프링 전용 인터페이스에 의존한다
+   * 초기화,소멸 메서드의 이름을 변경할 수 없다(인터페이스이므로 그대로 사용해야함)
+   * 내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다
+   */
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    System.out.println("NetworkClient.afterPropertiesSet");
+    connect();
+    call("초기화 연결 메시지");
+  }
+
+  @Override
+  public void destroy() throws Exception {
+    System.out.println("NetworkClient.destroy");
+    disconnect();
   }
 }
