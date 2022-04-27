ce40ab42e94b0516668472735781accda2fd3101
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-04-27 16:24:52 +0900

    :sparkles: request scope proxy mode 적용

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,9 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+// proxyMode = ScopedProxyMode.TARGET_CLASS를 사용하면 의존성 주입 때 가짜 proxy 객체를 먼저 넣어놓고
+// 실제 로직이 들어왔을 때 진짜를 찾아서 반환한다
 public class MyLogger {
 
   private String uuid;
